dd54010866afe54b896069b642c92bb0b05b1fcb
franzpaychatph <122615173+franzpaychatph@users.noreply.github.com>
2023-01-27 15:57:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,38 +24,34 @@ const ApiManager = require('./helpers/APIManager');
 const NotificationManager = require('./helpers/NotificationManager');
 const PartnerManager = require('./helpers/PartnerManager');
 const APISrvManager = require('./helpers/APISrvManager');
-const https = require('https');
+//FRANZ
+// const https = require('https');
 const socket = require('socket.io');
 const config = require('./config/config');
 const { instrument } = require('@socket.io/admin-ui');
 var fs = require( 'fs' );
-var sslRootCAs = require('ssl-root-cas');
+//FRANZ
+// var sslRootCAs = require('ssl-root-cas');
 
 var chat_srv_app = express();
 
 //FRANZ
-sslRootCAs.inject();
+// sslRootCAs.inject();
 var io = null;
 
-// Configure Chat Server Instance ------------------
-if(config.port == 443 || config.port == 2053) {
-    sslRootCAs.inject().addFile(config.certs.root);
-    var chat_server = https.createServer({
-        key: fs.readFileSync(config.certs.key),
-        cert: fs.readFileSync(config.certs.cert),
-        requestCert: false,
-        rejectUnauthorized: false
-    }, chat_srv_app);
-    chat_server.listen(config.port);
-    io = socket.listen(chat_server, {
-      allowEIO3: true,
-      cors: {
-        origin: ['https://admin.socket.io', 'http://localhost', 'http://localhost:3000', 'https://chatserver.paychat.ph'],
-        credentials: true
-      }
-    });
-    console.log('Listening...');
-} else {
+// // Configure Chat Server Instance ------------------
+// if(config.port == 443 || config.port == 2053) {
+//     sslRootCAs.inject().addFile(config.certs.root);
+//     var chat_server = https.createServer({
+//         key: fs.readFileSync(config.certs.key),
+//         cert: fs.readFileSync(config.certs.cert),
+//         requestCert: false,
+//         rejectUnauthorized: false
+//     }, chat_srv_app);
+//     chat_server.listen(config.port);
+//     io = socket.listen(chat_server);
+//     console.log('Listening...');
+// } else {
     var chat_server_unsecured = chat_srv_app.listen(config.port, () => {
     });      
     io = socket(chat_server_unsecured, {
@@ -66,7 +62,7 @@ if(config.port == 443 || config.port == 2053) {
         }
     });
     console.log(`Listening (unsecured ${config.port})...`);
-}
+// }
 
 //FRANZ
 // // Configure Cluster Server instance --------------
